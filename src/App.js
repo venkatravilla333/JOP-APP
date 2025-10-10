4e62d352100ae2b41dde9b6edce9dc67765ae73b
venkat ravilla <venkatravilla333@gmail.com>
2025-10-10 16:49:56 +0530

    redux-toolkit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@
 // import Parent from "./Props/Child-Parent/Parent"
 // import Parent from "./Props/Parent-Child/Parent"
 
-import A from "./4.State/Sync-Data/Global-State/Redux/react-components/A"
+import A from "./4.State/Sync-Data/Global-State/Redux-Toolkit/React-Components/A"
+
+// import A from "./4.State/Sync-Data/Global-State/Redux/react-components/A"
 
 // import A from "./4.State/Sync-Data/Global-State/Redux/A"
 
@@ -27,6 +29,7 @@ function App() {
     {/* <Parent/> */}
     {/* <Parent/> */}
     {/* <Parent/> */}
+    {/* <A/> */}
     {/* <A/> */}
     {/* <A/> */}
     {/* <A/> */}
